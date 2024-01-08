90ad950d451889e4b981003b0ebc389074376f89
Alex Tercete <alextercete@gmail.com>
2024-01-08 14:17:49 +0000

    Day 3, part 2

--- 03/gear_ratios.py
+++ 03/gear_ratios.py
@@ -1,18 +1,40 @@
 import fileinput
+from functools import reduce
 import re
 
 def part_numbers(numbers: dict, symbols: list):
     parts = []
     symbols_cache = set(symbols)
     for coordinates, number in numbers.items():
-        for neighbor in neighbors(coordinates, number):
+        for neighbor in neighbors(coordinates, len(str(number))):
             if neighbor in symbols_cache:
                 parts.append(number)
     return parts
 
-def neighbors(coordinates: tuple, number: int):
+def gear_ratios(numbers: dict, symbols: dict):
+    ratios = []
+    numbers_cache = create_numbers_cache(numbers)
+    gear_coordinates = [k for k, v in symbols.items() if v == "*"]
+    for coordinates in gear_coordinates:
+        parts = set()
+        for neighbor in neighbors(coordinates):
+            if neighbor in numbers_cache:
+                parts.add(numbers_cache[neighbor])
+        if len(parts) >= 2:
+            ratios.append(reduce(lambda a, b: a*b, parts))
+    return ratios
+
+def create_numbers_cache(numbers: dict):
+    cache = {}
+    for coordinates, number in numbers.items():
+        x, y = coordinates
+        length = len(str(number))
+        for i in range(x, x+length):
+            cache[(i, y)] = number
+    return cache
+
+def neighbors(coordinates: tuple, length=1):
     x, y = coordinates
-    length = len(str(number))
     for i in range(x-1, x+length+1):
         for j in range(y-1, y+2):
             if x <= i <= x+length-1 and j == y:
@@ -21,16 +43,16 @@ def neighbors(coordinates: tuple, number: int):
 
 def parse_engine_schematics(lines: list):
     numbers = {}
-    symbols = []
+    symbols = {}
     for j, line in enumerate(lines):
         for m in re.finditer(r"\d+", line):
             numbers[(m.start(), j)] = int(m.group(0))
         for m in re.finditer(r"[^.\d\s]", line):
-            symbols.append((m.start(), j))
+            symbols[(m.start(), j)] = m.group(0)
     return numbers, symbols
 
 if __name__ == "__main__":
     lines = fileinput.input()
     numbers, symbols = parse_engine_schematics(lines)
-    parts = part_numbers(numbers, symbols)
-    print(sum(parts))
+    ratios = gear_ratios(numbers, symbols)
+    print(sum(ratios))
--- 03/test_gear_ratios.py
+++ 03/test_gear_ratios.py
@@ -1,4 +1,4 @@
-from gear_ratios import parse_engine_schematics, part_numbers
+from gear_ratios import parse_engine_schematics, part_numbers, gear_ratios
 
 def test_single_digit_part_numbers():
     numbers = {
@@ -24,6 +24,34 @@ def test_multiple_digit_part_numbers():
     ]
     assert part_numbers(numbers, symbols) == [12, 345]
 
+def test_gear_ratios_horizontal():
+    numbers = {
+        (0, 0): 12,
+        (3, 0): 345,
+        (9, 0): 6
+    }
+    symbols = {
+        (2, 0): "*",
+        (7, 1): "*"
+    }
+    assert gear_ratios(numbers, symbols) == [
+        12*345
+    ]
+
+def test_gear_ratios_vertical():
+    numbers = {
+        (0, 0): 12,
+        (0, 2): 345,
+        (0, 5): 6
+    }
+    symbols = {
+        (0, 1): "*",
+        (0, 6): "*"
+    }
+    assert gear_ratios(numbers, symbols) == [
+        12*345
+    ]
+
 def test_parse_engine_schematics():
     lines = [
         "123..*45\n",
@@ -36,7 +64,7 @@ def test_parse_engine_schematics():
         (3, 1): 67,
         (7, 1): 8
     }
-    assert symbols == [
-        (5, 0),
-        (1, 1)
-    ]
+    assert symbols == {
+        (5, 0): "*",
+        (1, 1): "$"
+    }
